6ccb305e60c344e972fd2a769bacdb6920e8aea4
neyaznafiz <neyaznafiz@outlook.com>
2023-04-15 10:52:39 +0600

    environment

--- express-app/main.js
+++ express-app/main.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express()
-const os = require('os')
+const express = require("express");
+const app = express();
+const os = require("os");
 
-const port = 5000
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hostname: ' + os.hostname())
-})
+app.get("/", (req, res) => {
+  res.send("Hostname: " + os.hostname());
+});
+
+app.get("/env", (req, res) => {
+  res.send("MY ENV: " + process.env.DB_USERNAME);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
